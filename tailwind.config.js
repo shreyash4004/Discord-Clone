7a26f5f62d086c8d4b4068b9fcc67d02b3682e77
Shreyash Bandi <shreyashbandi19@gmail.com>
2023-06-07 12:41:09 +0530

    Discord Clone Website

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+        fontFamily:{
+        mullish:["Mullish","san-serif"],
+              },
+              colors: {
+                deepBlue: "#02042a",
+                lightBlue: "#2b84ea",
+                lightBlue: "#2b84ea",
+                lightBlue300: "#4b94ed",
+                lightBlue500: "#0b72e7",
+                greenLight: "#61cea6",
+                grayText: "#818597",
+                lightGray: "#e2e2e2",
+                grayBlue: "#344a6c",
+                deepBlueHead: "#162f56",
+                gray2: "#525a76",
+         
+              },
+    },
+  },
+  plugins: [],
+  fontFamily:{
+    Whitney: ["Whitney"],
+    Ginto: ["Ginto"], 
+    ggSans: ["ggSans"],
+    },
+}
+
